239d872f336786a2e781dedea9dc867afe18bd95
Peter Tran-Truong <peter.dai.truong@gmail.com>
2023-04-21 14:40:23 -0700

    Update README.md
    - Reformat code

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const mainContainer = document.getElementsByClassName("main-container");
+const mainContainer = document.getElementsByClassName('main-container');
 const onResize = (event) => {
-    if (mainContainer.length == 1 && window.innerWidth < window.innerHeight) {
-        mainContainer[0].style.flexDirection = 'column-reverse';
-    } else {
-        mainContainer[0].style.flexDirection = 'row';
-    }
-}
+  if (mainContainer.length == 1 && window.innerWidth < window.innerHeight) {
+    mainContainer[0].style.flexDirection = 'column-reverse';
+  } else {
+    mainContainer[0].style.flexDirection = 'row';
+  }
+};
 
 (function () {
-    addEventListener("resize", onResize);
+  addEventListener('resize', onResize);
 })();
